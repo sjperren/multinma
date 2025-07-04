26c90debfae75c44d86b3017f3c77d0b2a816ad9
Sam Perren <ad21883@bristol.ac.uk>
2025-07-04 14:42:03 +0100

    First attempt at test that fixed baselines

--- tests/testthat/test-connect_baseline.R
+++ tests/testthat/test-connect_baseline.R
@@ -32,3 +32,29 @@ test_that("nma connects baselines", {
     NA
   )
 })
+
+test_that("fixed baseline connection merges studies", {
+  spec <- con("fixed", studies = levels(net$studies)[1:2])
+  fit <- nma(net, connect_baseline = spec,
+             prior_intercept = normal(scale = 10),
+             prior_trt = normal(scale = 10),
+             iter = 1, chains = 1)
+  expect_true(any(grepl("&", levels(fit$network$studies))))
+})
+
+test_that("fixed baseline connection not allowed for contrast data", {
+  contr_net <- set_agd_contrast(parkinsons,
+                                study = studyn,
+                                trt = trtn,
+                                y = diff,
+                                se = se_diff,
+                                sample_size = n)
+  spec <- con("fixed", studies = levels(contr_net$studies)[1:2])
+  expect_error(
+    nma(contr_net, connect_baseline = spec,
+        prior_intercept = normal(scale = 10),
+        prior_trt = normal(scale = 10),
+        iter = 1, chains = 1),
+    "AgD contrast data"
+  )
+})
