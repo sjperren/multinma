b8f7e0371440fd9136f118681826965eb9defe52
Sam Perren <ad21883@bristol.ac.uk>
2025-07-16 13:59:40 +0100

    correction

--- R/nma.R
+++ R/nma.R
@@ -1905,6 +1905,8 @@ if (class_effects == "exchangeable") {
                                 "Cauchy",  "half-Cauchy",
                                 "Student t", "half-Student t", "log-Student t",
                                 "Exponential", "flat (implicit)")),
+    prior_het_type = switch(prior_het_type,
+                            sd = 1, var = 2, prec = 3)
     !!! prior_standat(which_BP, "prior_baseline",
                       valid = c("Normal", "Cauchy", "Student t", "flat (implicit)"))
     )
