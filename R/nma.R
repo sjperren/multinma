1c906a4198fe1909924fb85e886595dee01a58f5
Sam Perren <ad21883@bristol.ac.uk>
2025-09-09 12:36:27 +0100

    stan and R code for random baselines

--- R/nma.R
+++ R/nma.R
@@ -1508,7 +1508,9 @@ nma.fit <- function(ipd_x, ipd_y,
                     adapt_delta = NULL,
                     int_thin = 0,
                     int_check = TRUE,
-                    basis) {
+                    basis,
+                    random_baseline = FALSE,
+                    prior_intercept_sd) {
 
   if (missing(ipd_x)) ipd_x <- NULL
   if (missing(ipd_y)) ipd_y <- NULL
@@ -1622,6 +1624,12 @@ if (class_effects == "exchangeable") {
 
   # Check priors
   check_prior(prior_intercept)
+  if (random_baseline == TRUE){
+    check_prior(prior_intercept_sd)
+  } else {
+    # Dummy intercept priors for fixed baseline models, not used but requested by Stan data
+    prior_intercept_sd <- half_normal(1)
+  }
   check_prior(prior_trt)
   if (trt_effects == "random") check_prior(prior_het)
   check_prior(prior_reg)
@@ -1824,8 +1832,8 @@ if (class_effects == "exchangeable") {
     agd_contrast_trt_b = as.array(agd_contrast_trt_b),
     agd_contrast_y = if (has_agd_contrast) as.array(agd_contrast_y$.y) else numeric(),
     agd_contrast_Sigma = Sigma,
-    # ipd_study = ipd_study,
-    # agd_arm_study = agd_arm_study,
+    ipd_study = ipd_study,
+    agd_arm_study = agd_arm_study,
     # agd_contrast_study = agd_contrast_study,
     # Random effects
     RE = switch(trt_effects, fixed = 0, random = 1),
@@ -1843,8 +1851,10 @@ if (class_effects == "exchangeable") {
     # Class effects
     which_CE = if (class_effects == "exchangeable") which_CE else numeric(0),
     which_CE_sd = if (class_effects == "exchangeable") which_CE_sd else numeric(0),
-    class_effects = ifelse(class_effects == "exchangeable", 1, 0)
-    )
+    class_effects = ifelse(class_effects == "exchangeable", 1, 0),
+    #random baseline effect
+    random_baseline = ifelse(random_baseline == TRUE, 1, 0)
+  )
 
   # Add priors
   standat <- purrr::list_modify(standat,
@@ -1898,6 +1908,11 @@ if (class_effects == "exchangeable") {
   # Monitor class effects if class effects in use
   if (class_effects == "exchangeable") {
     pars <- c(pars, "class_mean", "class_sd")
+  }
+
+  # Monitor baseline mean, sd and new if random baselines in use
+  if (random_baseline == TRUE) {
+    pars <- c(pars, "baseline_mean", "baseline_sd", "baseline_new")
   }
 
   # Set adapt_delta, but respect other control arguments if passed in ...
@@ -2477,6 +2492,12 @@ if (class_effects == "exchangeable") {
   }
   fnames_oi <- gsub("tau[1]", "tau", fnames_oi, fixed = TRUE)
   fnames_oi <- gsub("omega[1]", "omega", fnames_oi, fixed = TRUE)
+
+  if (random_baseline == TRUE) {
+    fnames_oi <- gsub("baseline_mean[1]", "baseline_mean", fnames_oi, fixed = TRUE)
+    fnames_oi <- gsub("baseline_sd[1]", "baseline_sd", fnames_oi, fixed = TRUE)
+    fnames_oi <- gsub("baseline_new[1]", "baseline_new", fnames_oi, fixed = TRUE)
+  }
 
 
   if (likelihood == "ordered") {
